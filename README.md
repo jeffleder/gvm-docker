fc7b9b2068e3eb203371342b19fea00731582c38
Jeff Leder <lederjeff@gmail.com>
2021-03-03 17:17:31 -0600

    Updated to version 20.8

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # GVM-Docker
-This is a GVM11 (OpenVAS) Docker Image intended for one-off scans.
-Inspiration for this repo comes from the following:
-* https://github.com/immauss/openvas
-* https://kifarunix.com/install-and-setup-gvm-11-on-ubuntu-20-04/
+This is a GVM (OpenVAS) Docker image intended for one-off scans.
 
 # Docker Hub
 https://hub.docker.com/repository/docker/jeffleder/gvm-docker
@@ -15,16 +12,16 @@ docker volume create openvas
 ```
 3. Run the container
 ```shell
-docker run -d -p 127.0.0.1:9392:9392 -e USERNAME="admin" -e PASSWORD="admin" --volume openvas:/opt/gvm --name openvas --restart unless-stopped jeffleder/gvm-docker
+docker run --detach --publish 127.0.0.1:9392:9392 -e USERNAME="admin" -e PASSWORD="admin" --volume openvas:/opt/gvm --name openvas --restart unless-stopped jeffleder/gvm-docker
 ```
 # Deployment Notes
-* The `-d` switch will run the container in a detached state
-* The `-p 127.0.0.1:9392:9392` switch will forward host port `9392` to container port `9392` (the GSA web GUI port)
+* The `--detach` switch will run the container in a detached state
+* The `--publish 127.0.0.1:9392:9392` switch will forward host port `9392` to container port `9392` (the GSA web GUI port)
    * You can change the host port from `9392` to any available port that you'd like (e.g. `8080`)
-   * The `127.0.0.1` part of the `-p 127.0.0.1:9392:9392` switch will force GSA to only listen for localhost connections
-* The `-e USERNAME="admin"` switch allows you to sepcify a username to log into the GSAD web GUI with
-* The `-e PASSWORD="admin"` switch allows you to sepcify a password to log into the GSAD web GUI with
-* The `-e PASSWORD="admin"` switch allows you to sepcify a password to log into the GSAD web GUI with
+   * The `127.0.0.1` part of the switch will force GSA to only listen for localhost connections
+* The `-e USERNAME="admin"` switch allows you to specify a username to log into the GSAD web GUI with
+* The `-e PASSWORD="admin"` switch allows you to specify a password to log into the GSAD web GUI with
+* The `--volume` switch specifies the vilume to use for persistent data
 * The `--name` switch allows you to specify whatever freindly name you want for the container (this guide uses `openvas` throughout)
 * The `--restart unless-stopped` switch daemonizes the container (so it will restart on crash and/or system reboots)
 * The last `jeffleder/gvm-docker` item specifies the image to pull and run for the container
@@ -34,7 +31,7 @@ You can verify that the container has completed loading by running:
 ```
 docker logs openvas
 ```
-If you see `Your GVM 11 container is now ready to use!`, the container is ready to use.
+If you see `Your GVM container is now ready to use!`, the container is ready to go.
 
 # Accessing the Web GUI
 Access the web interface from the docker host via `http://127.0.0.1:9392`
@@ -51,10 +48,10 @@ docker top openvas
 ```
 
 # Checking the GVM Logs
-All the logs from /opt/gvm/var/log/gvm* can be viewed by running:
+All the logs from /var/log/gvm/* can be viewed by running:
 ```
 docker logs openvas
 ```
 
 # Updating NVT/SCAP/CERT Data
-Autobuilds should bake NVT/SCAP/CERT Data into the DockerHub image so that the latest builds have the most recent data
+Autobuilds bake NVT/SCAP/CERT Data into the DockerHub image so that the latest build has the most recent feeds
--- start.sh
+++ start.sh
@@ -2,29 +2,42 @@
 set -Eeuo pipefail
 USERNAME=${USERNAME:-admin}
 PASSWORD=${PASSWORD:-admin}
-echo 'Starting Redis'
-redis-server /etc/redis/redis-openvas.conf
-while [ ! -S /run/redis-openvas/redis.sock ];do echo '--> Waiting for redis socket';sleep 1;done;
-while [ "$(redis-cli -s /run/redis-openvas/redis.sock ping)" != 'PONG' ];do echo '--> Waiting for redis to respond';sleep 1;done;
+###############################################################################################
+echo 'Starting Redis' #service redis-server@openvas start
+mkdir -p /run/redis-openvas/
+chown redis:redis /run/redis-openvas/
+chmod 755 /run/redis-openvas/
+touch /var/log/redis/redis-server-openvas.log
+chmod 777 /var/log/redis/redis-server-openvas.log
+runuser -u redis -- redis-server /etc/redis/redis-openvas.conf
+while [ ! -S /run/redis-openvas/redis-server.sock ];do echo '--> Waiting for redis-server socket';sleep 1;done;
+while [ "$(redis-cli -s /run/redis-openvas/redis-server.sock ping)" != 'PONG' ];do echo '--> Waiting for redis to respond';sleep 1;done;
+###############################################################################################
 echo 'Starting PostgreSQL'
-/usr/bin/pg_ctlcluster --skip-systemctl-redirect 12 main start
-echo 'Starting OSPd'
-if [ -f /opt/gvm/var/run/feed-update.lock ];then echo '--> Removing stale feed-update.lock file';rm /opt/gvm/var/run/feed-update.lock;fi;
-if [ -f /opt/gvm/var/run/ospd-openvas.pid ];then echo '--> Removing stale ospd-openvas.pid file';rm /opt/gvm/var/run/ospd-openvas.pid;fi;
-if [ -S /opt/gvm/var/run/ospd.sock ];then echo '--> Removing stale ospd.sock file';rm /opt/gvm/var/run/ospd.sock;fi;
-ospd-openvas --pid-file /opt/gvm/var/run/ospd-openvas.pid --log-file /opt/gvm/var/log/gvm/ospd-openvas.log --lock-file-dir /opt/gvm/var/run -u /opt/gvm/var/run/ospd.sock
-while [ ! -S /opt/gvm/var/run/ospd.sock ];do echo '--> Waiting for OSPd socket';sleep 10;done;
-echo 'Starting GVMd'
-gvmd --osp-vt-update=/opt/gvm/var/run/ospd.sock
-until gvmd --get-users;do echo '--> Waiting for GVMd socket';sleep 10;done;
+service postgresql start
+###############################################################################################
+echo 'Starting OSPd' #service ospd-openvas start
+if [ -f /var/lib/openvas/feed-update.lock ];then echo '--> Removing stale feed-update.lock file';rm /var/lib/openvas/feed-update.lock;fi;
+if [ -f /run/ospd/ospd-openvas.pid ];then echo '--> Removing stale ospd-openvas.pid file';rm /run/ospd/ospd-openvas.pid;fi;
+if [ -S /run/ospd/ospd.sock ];then echo '--> Removing stale ospd.sock file';rm /run/ospd/ospd.sock;fi;
+mkdir -p /run/ospd/
+chown _gvm:_gvm /run/ospd/
+runuser -u _gvm -- ospd-openvas --unix-socket /run/ospd/ospd.sock --pid-file /run/ospd/ospd-openvas.pid --log-file /var/log/gvm/ospd-openvas.log --lock-file-dir /var/lib/openvas
+while [ ! -S /run/ospd/ospd.sock ];do echo '--> Waiting for OSPd socket';sleep 10;done;
+###############################################################################################
+echo 'Starting GVMd' #service gvmd start
+mkdir -p /run/gvm/
+chown _gvm:_gvm /run/gvm/
+chmod 775 /run/gvm/
+runuser -u _gvm -- gvmd --osp-vt-update=/run/ospd/ospd.sock
+###############################################################################################
+mkdir -p /data/
 if [ ! -f '/data/created_gvm_user' ];then echo 'Creating admin user';gvmd --create-user=${USERNAME} --password=${PASSWORD};touch /data/created_gvm_user;fi;
-echo 'Starting GSA'
-gsad --verbose --http-only --no-redirect --port=9392
-echo '++++++++++++++++++++++++++++++++++++++++++++++'
-echo '+ Your GVM 11 container is now ready to use! +'
-echo '++++++++++++++++++++++++++++++++++++++++++++++'
-echo ''
-echo '++++++++++++++++'
-echo '+ Tailing logs +'
-echo '++++++++++++++++'
-tail -F /opt/gvm/var/log/gvm/*
+###############################################################################################
+echo 'Starting GSA' #service gsad start
+runuser -u _gvm -- gsad  --verbose --http-only --no-redirect --port=9392
+###############################################################################################
+echo '+++++++++++++++++++++++++++++++++++++++++++'
+echo '+ Your GVM container is now ready to use! +'
+echo '+++++++++++++++++++++++++++++++++++++++++++'
+tail -F /var/log/gvm/*
