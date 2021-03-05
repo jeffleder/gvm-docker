8649f5d134198836dcb609dc69af31fe58727563
Jeff Leder <lederjeff@gmail.com>
2021-03-05 12:22:33 -0600

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,16 @@
-#REF:https://www.agix.com.au/installing-openvas-on-kali-in-2020/
 FROM debian:bullseye
-#update sources.list (for greenbone-security-assistant)
 RUN sed -i.bak 's/ main/ main contrib non-free/g' /etc/apt/sources.list
 RUN apt-get -y -qq update >/dev/null
+RUN apt-get -y -qq install net-tools >/dev/null
+RUN apt-get -y -qq install procps >/dev/null
+RUN apt-get -y -qq install nano >/dev/null
 RUN apt-get -y -qq install gvm >/dev/null
 RUN apt-get -y -qq install greenbone-security-assistant >/dev/null
-#switch postgresql service start syntax in /usr/bin/gvm-setup
 RUN sed -i.bak 's/systemctl start postgresql/service postgresql start/g' /usr/bin/gvm-setup
-#disable redis service start in /usr/bin/gvm-feed-update (launched at beginning of next step instead)
 RUN sed -i.bak 's/systemctl start redis-server@openvas.service/#systemctl start redis-server@openvas.service/g' /usr/bin/gvm-feed-update
-RUN echo 'Starting Redis and running setup' && \
-    mkdir -p /run/redis-openvas/ && chown redis:redis /run/redis-openvas/ && chmod 755 /run/redis-openvas/ && \
-    touch /var/log/redis/redis-server-openvas.log && chmod 777 /var/log/redis/redis-server-openvas.log && \
-    runuser -u redis -- redis-server /etc/redis/redis-openvas.conf && \
-	gvm-setup >/dev/null
-COPY start.sh /
-RUN chmod +x /start.sh
-CMD '/start.sh'
+RUN install --directory --owner=redis --group=redis --mode=777 /run/redis-openvas/
+RUN touch /var/log/redis/redis-server-openvas.log && chmod 777 /var/log/redis/redis-server-openvas.log
+RUN runuser -u redis -- redis-server /etc/redis/redis-openvas.conf && gvm-setup
+COPY launch.sh /
+RUN chmod +x /launch.sh
+CMD /launch.sh
---
+++ launch.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+USERNAME=${USERNAME:-admin}
+PASSWORD=${PASSWORD:-admin}
+###############################################################################################
+echo 'Starting Redis' #service redis-server@openvas start
+runuser -u redis -- redis-server /etc/redis/redis-openvas.conf
+while [ ! -S /run/redis-openvas/redis-server.sock ];do echo 'Waiting for redis-server socket';sleep 1;done;
+while [ "$(redis-cli -s /run/redis-openvas/redis-server.sock ping)" != 'PONG' ];do echo 'Waiting for redis to respond';sleep 1;done;
+###############################################################################################
+echo 'Starting PostgreSQL'
+service postgresql start
+###############################################################################################
+echo 'Starting OSPd' #service ospd-openvas start
+if [ -f /var/lib/openvas/feed-update.lock ];then echo 'Removing stale feed-update.lock file';rm /var/lib/openvas/feed-update.lock;fi;
+if [ -f /run/ospd/ospd-openvas.pid ];then echo 'Removing stale ospd-openvas.pid file';rm /run/ospd/ospd-openvas.pid;fi;
+if [ -S /run/ospd/ospd.sock ];then echo 'Removing stale ospd.sock file';rm /run/ospd/ospd.sock;fi;
+install --directory --owner=_gvm --group=_gvm --mode=777 /run/ospd/
+runuser -u _gvm -- ospd-openvas --unix-socket /run/ospd/ospd.sock --pid-file /run/ospd/ospd-openvas.pid --log-file /var/log/gvm/ospd-openvas.log --lock-file-dir /var/lib/openvas
+while [ ! -S /run/ospd/ospd.sock ];do echo 'Waiting for OSPd socket';sleep 10;done;
+###############################################################################################
+echo 'Starting GVMd' #service gvmd start
+install --directory --owner=_gvm --group=_gvm --mode=777 /run/gvm/
+runuser -u _gvm -- gvmd --osp-vt-update=/run/ospd/ospd.sock
+###############################################################################################
+echo 'Starting GSA' #service gsad start
+runuser -u _gvm -- gsad  --verbose --http-only --no-redirect --port=9392
+###############################################################################################
+runuser -u _gvm -- gvmd --create-user=${USERNAME} --password=${PASSWORD}
+###############################################################################################
+echo '+++++++++++++++++++++++++++++++++++++++++++'
+echo '+ Your GVM container is now ready to use! +'
+echo '+++++++++++++++++++++++++++++++++++++++++++'
+tail -F /var/log/gvm/*
